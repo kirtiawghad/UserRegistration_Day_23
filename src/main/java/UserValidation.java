e661feff52c8fb8559cc57ab9e331e12f1e5f6f7
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-29 16:38:30 +0530

    Added:UC2

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -2,10 +2,17 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class UserValidation {
     static Pattern firstName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
+    static Pattern lastName = Pattern.compile("^([A-Z]{1}+)([a-z]{2,}+)$");
 
     public boolean validateFirstName(String testFirstName){
         Matcher matcher = firstName.matcher(testFirstName);
         return matcher.matches();
     }
+
+    public boolean validateLastName(String testLastName){
+        Matcher matcher = lastName.matcher(testLastName);
+        return matcher.matches();
+    }
+
 
 }
